31d93959428aa274efb7cc4894c61e601f23476c
Vlad Kov <vladkov135@gmail.com>
2024-11-17 18:07:39 +0300

    Main file and logger have been created

---
+++ logger.py
@@ -0,0 +1,23 @@
+import logging
+from logging.handlers import RotatingFileHandler
+
+from config import LOGS_PATH
+
+
+def configure_logging(level=logging.WARNING):
+    max_size = 5 * 1025 * 1024 # 5 MB
+
+    handler = RotatingFileHandler(LOGS_PATH, maxBytes=max_size, backupCount=3)
+    logging.basicConfig(level=level,
+                        format='[%(asctime)s.%(msecs)03d] %(levelname)-7s %(module)-20s: %(lineno)-3d - %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S',
+                        handlers=[handler])
+
+
+if __name__ == '__main__':
+    logger = logging.getLogger(__name__)
+    configure_logging(level=logging.DEBUG)
+    number = 5
+    logger.warning('warrning message number: %s',number)
+    logger.info('info message')
+    logger.debug('debug message')
---
+++ main.py
@@ -0,0 +1,29 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+import logging
+
+from config import TOKEN
+from logger import configure_logging
+
+print('Running')
+
+logger = logging.getLogger(__name__)
+configure_logging(logging.DEBUG)
+
+try:
+    bot = Bot(token=TOKEN)
+    dp = Dispatcher()
+except Exception as e:
+    logger.exception(e)
+
+
+async def main():
+    logger.info('Start')
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        logger.info('Exit')
+        print('Exit')
